0b51c390baa58039ce28dc78af562c85d4637a98
Albert Nigmatullin <albertnig@rambler.ru>
2024-01-12 15:39:15 +0300

    initial commit

---
+++ KazanBus/src/main/java/io/proj3ct/KazanBus/KazanBusApplication.java
@@ -0,0 +1,13 @@
+package io.proj3ct.KazanBus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KazanBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KazanBusApplication.class, args);
+	}
+
+}
---
+++ KazanBus/src/test/java/io/proj3ct/KazanBus/KazanBusApplicationTests.java
@@ -0,0 +1,13 @@
+package io.proj3ct.KazanBus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KazanBusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
